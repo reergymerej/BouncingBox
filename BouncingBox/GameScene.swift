760754aa20114d68835c379af03fec9b4672036c
Jeremy Greer <jex.grizzle@gmail.com>
2018-08-11 21:22:26 -0400

    Add another box

--- BouncingBox/GameScene.swift
+++ BouncingBox/GameScene.swift
@@ -11,79 +11,78 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var squareNode = SKShapeNode()
+    private var squareNode2 = SKShapeNode()
+    private var xSpeed = CGFloat(3.0)
+    private var ySpeed = CGFloat(3.0)
+    private var xSpeed2 = CGFloat(2.0)
+    private var ySpeed2 = CGFloat(-3.5)
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        squareNode = SKShapeNode(rect: CGRect(x: 0, y: 0, width: 50, height: 50))
+        addChild(squareNode)
+
+        squareNode2 = SKShapeNode(rect: CGRect(x: 0, y: 0, width: 50, height: 50))
+        squareNode2.position.x = 100
+        squareNode2.position.y = size.height - 50
+        addChild(squareNode2)
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
+
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        // calculate new position
+        var nextX = squareNode.position.x + xSpeed
+        var nextY = squareNode.position.y + ySpeed
+        var nextX2 = squareNode2.position.x + xSpeed2
+        var nextY2 = squareNode2.position.y + ySpeed2
+        
+        // handle collisions
+        let right = size.width - 50
+        let left = CGFloat(0)
+        let top = size.height - 50
+        let bottom = CGFloat(0)
+        
+        if nextX > right {
+            nextX = right
+            xSpeed = -xSpeed
+        }
+        if nextX < left {
+            nextX = left
+            xSpeed = -xSpeed
+        }
+        
+        if nextY > top {
+            nextY = top
+            ySpeed = -ySpeed
+        }
+        if nextY < bottom {
+            nextY = bottom
+            ySpeed = -ySpeed
+        }
+        
+        //
+        
+        if nextX2 > right {
+            nextX2 = right
+            xSpeed2 = -xSpeed2
+        }
+        if nextX2 < left {
+            nextX2 = left
+            xSpeed2 = -xSpeed2
+        }
+        
+        if nextY2 > top {
+            nextY2 = top
+            ySpeed2 = -ySpeed2
+        }
+        if nextY2 < bottom {
+            nextY2 = bottom
+            ySpeed2 = -ySpeed2
+        }
+
+        // update position
+        squareNode.position.x = nextX
+        squareNode.position.y = nextY
+        squareNode2.position.x = nextX2
+        squareNode2.position.y = nextY2
     }
 }
--- BouncingBox/GameViewController.swift
+++ BouncingBox/GameViewController.swift
@@ -14,21 +14,15 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
+
+        // I don't know why I need this.
+        if let view = self.view as? SKView {
+            let scene = GameScene(size: self.view.bounds.size)
+            scene.scaleMode = .resizeFill
             view.ignoresSiblingOrder = true
-            
             view.showsFPS = true
             view.showsNodeCount = true
+            view.presentScene(scene)
         }
     }
 

1b48bd3e8e13e1d41e57215bfd31d4fef22563f9
Jeremy Greer <jex.grizzle@gmail.com>
2018-08-12 21:36:11 -0400

    Add boxes on click

---
+++ BouncingBox/Box.swift
@@ -0,0 +1,100 @@
+//
+//  Box.swift
+//  BouncingBox
+//
+//  Created by Jeremy Greer on 8/12/18.
+//  Copyright © 2018 Grizzle. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Box {
+    var node : SKShapeNode
+    var half : CGFloat
+    var xSpeed : CGFloat
+    var ySpeed : CGFloat
+    
+    init(size: CGFloat, xSpeed: CGFloat, ySpeed: CGFloat, position: CGPoint) {
+        self.xSpeed = xSpeed
+        self.ySpeed = ySpeed
+        self.half = size / 2
+        node = SKShapeNode(rect: CGRect(x: -half, y: -half, width: size, height: size))
+        node.position = position
+    }
+    
+    func setX(x: CGFloat) {
+        node.position.x = x
+    }
+    
+    func setY(y: CGFloat) {
+        node.position.y = y
+    }
+    
+    func move() {
+        node.position = CGPoint(x: node.position.x + xSpeed,
+                                y: node.position.y + ySpeed)
+    }
+    
+    func maybeBounceLeft() -> Bool {
+        let x = node.position.x
+        let limit = CGFloat(0) + half
+        let beyond = limit - x
+        if beyond > 0 {
+            node.position.x = limit + beyond
+            return true
+        }
+        return false
+    }
+    
+    func maybeBounceRight(right: CGFloat) -> Bool {
+        let x = node.position.x
+        let limit = right - half
+        let beyond = x - limit
+        if beyond > 0 {
+            node.position.x = limit - beyond
+            return true
+        }
+        return false
+    }
+    
+    func maybeBounceX(right: CGFloat) {
+        if maybeBounceLeft() || maybeBounceRight(right: right) {
+            xSpeed = -xSpeed
+        }
+    }
+    
+    func maybeBounceBottom() -> Bool {
+        let y = node.position.y
+        let limit = CGFloat(0) + half
+        let beyond = limit - y
+        if beyond > 0 {
+            node.position.y = limit + beyond
+            return true
+        }
+        return false
+    }
+    
+    func maybeBounceTop(top: CGFloat) -> Bool {
+        let y = node.position.y
+        let limit = top - half
+        let beyond = y - limit
+        if beyond > 0 {
+            node.position.y = limit - beyond
+            return true
+        }
+        return false
+    }
+    
+    func maybeBounceY(top: CGFloat) {
+        if maybeBounceBottom() || maybeBounceTop(top: top) {
+            ySpeed = -ySpeed
+        }
+    }
+    
+    
+    func maybeBounce(top: CGFloat, right: CGFloat) {
+        maybeBounceX(right: right)
+        maybeBounceY(top: top)
+    }
+}
--- BouncingBox/GameScene.swift
+++ BouncingBox/GameScene.swift
@@ -9,126 +9,10 @@
 import SpriteKit
 import GameplayKit
 
-class Box {
-    var node : SKShapeNode
-    var size : CGFloat
-    var xSpeed : CGFloat
-    var ySpeed : CGFloat
-    
-    init(size: CGFloat, xSpeed: CGFloat, ySpeed: CGFloat, position: CGPoint) {
-        self.size = size
-        self.xSpeed = xSpeed
-        self.ySpeed = ySpeed
-        node = SKShapeNode(rect: CGRect(x: 0, y: 0, width: size, height: size))
-        node.position = position
-    }
-    
-    func setX(x: CGFloat) {
-        node.position.x = x
-    }
-    
-    func setY(y: CGFloat) {
-        node.position.y = y
-    }
-    
-    func move() {
-        node.position = CGPoint(x: node.position.x + xSpeed,
-                                y: node.position.y + ySpeed)
-    }
-    
-    func maybeBounceLeft() -> Bool {
-        let x = node.position.x
-        let limit = CGFloat(0)
-        let beyond = limit - x
-        if beyond > 0 {
-            node.position.x = limit + beyond
-            return true
-        }
-        return false
-    }
-    
-    func maybeBounceRight(right: CGFloat) -> Bool {
-        let x = node.position.x
-        let limit = right - size
-        let beyond = x - limit
-        if beyond > 0 {
-            node.position.x = limit - beyond
-            return true
-        }
-        return false
-    }
-    
-    func maybeBounceX(right: CGFloat) {
-        if maybeBounceLeft() || maybeBounceRight(right: right) {
-            xSpeed = -xSpeed
-        }
-    }
-    
-    func maybeBounceBottom() -> Bool {
-        let y = node.position.y
-        let limit = CGFloat(0)
-        let beyond = limit - y
-        if beyond > 0 {
-            node.position.y = limit + beyond
-            return true
-        }
-        return false
-    }
-    
-    func maybeBounceTop(top: CGFloat) -> Bool {
-        let y = node.position.y
-        let limit = top - size
-        let beyond = y - limit
-        if beyond > 0 {
-            node.position.y = limit - beyond
-            return true
-        }
-        return false
-    }
-    
-    func maybeBounceY(top: CGFloat) {
-        if maybeBounceBottom() || maybeBounceTop(top: top) {
-            ySpeed = -ySpeed
-        }
-    }
-
-
-    func maybeBounce(top: CGFloat, right: CGFloat) {
-        maybeBounceX(right: right)
-        maybeBounceY(top: top)
-    }
-}
-
 class GameScene: SKScene {
     private var boxes : [Box] = []
     
     override func didMove(to view: SKView) {
-        var box = Box(
-            size: CGFloat(50),
-            xSpeed: CGFloat(3.0),
-            ySpeed: CGFloat(3.0),
-            position: CGPoint(x: 0, y: 0)
-        )
-        addChild(box.node)
-        boxes.append(box)
-        
-        box = Box(
-            size: CGFloat(50),
-            xSpeed: CGFloat(2.0),
-            ySpeed: CGFloat(1.3),
-            position: CGPoint(x: 80, y: 0)
-        )
-        addChild(box.node)
-        boxes.append(box)
-        
-        box = Box(
-            size: CGFloat(33),
-            xSpeed: CGFloat(3.0),
-            ySpeed: CGFloat(3.3),
-            position: CGPoint(x: 23, y: 90)
-        )
-        addChild(box.node)
-        boxes.append(box)
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -140,4 +24,24 @@ class GameScene: SKScene {
             box.maybeBounce(top: top, right: right)
         }
     }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            addBox(location: touch.location(in: self.view))
+        }
+    }
+    
+    func addBox(location: CGPoint) {
+        let x = location.x
+        let y = size.height - location.y
+        let box = Box(
+            size: CGFloat(50),
+            xSpeed: CGFloat(3.0),
+            ySpeed: CGFloat(3.0),
+            position: CGPoint(x: x, y: y)
+        )
+        addChild(box.node)
+        boxes.append(box)
+    }
+    
 }

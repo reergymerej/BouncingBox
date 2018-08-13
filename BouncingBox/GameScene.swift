d79c47ea30eaf86e52b857ad613b1a72ef82dc37
Jeremy Greer <jex.grizzle@gmail.com>
2018-08-12 21:14:28 -0400

    Loop over boxes

--- BouncingBox/GameScene.swift
+++ BouncingBox/GameScene.swift
@@ -9,80 +9,135 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class Box {
+    var node : SKShapeNode
+    var size : CGFloat
+    var xSpeed : CGFloat
+    var ySpeed : CGFloat
     
-    private var squareNode = SKShapeNode()
-    private var squareNode2 = SKShapeNode()
-    private var xSpeed = CGFloat(3.0)
-    private var ySpeed = CGFloat(3.0)
-    private var xSpeed2 = CGFloat(2.0)
-    private var ySpeed2 = CGFloat(-3.5)
+    init(size: CGFloat, xSpeed: CGFloat, ySpeed: CGFloat, position: CGPoint) {
+        self.size = size
+        self.xSpeed = xSpeed
+        self.ySpeed = ySpeed
+        node = SKShapeNode(rect: CGRect(x: 0, y: 0, width: size, height: size))
+        node.position = position
+    }
     
-    override func didMove(to view: SKView) {
-        squareNode = SKShapeNode(rect: CGRect(x: 0, y: 0, width: 50, height: 50))
-        addChild(squareNode)
-
-        squareNode2 = SKShapeNode(rect: CGRect(x: 0, y: 0, width: 50, height: 50))
-        squareNode2.position.x = 100
-        squareNode2.position.y = size.height - 50
-        addChild(squareNode2)
+    func setX(x: CGFloat) {
+        node.position.x = x
+    }
+    
+    func setY(y: CGFloat) {
+        node.position.y = y
+    }
+    
+    func move() {
+        node.position = CGPoint(x: node.position.x + xSpeed,
+                                y: node.position.y + ySpeed)
+    }
+    
+    func maybeBounceLeft() -> Bool {
+        let x = node.position.x
+        let limit = CGFloat(0)
+        let beyond = limit - x
+        if beyond > 0 {
+            node.position.x = limit + beyond
+            return true
+        }
+        return false
+    }
+    
+    func maybeBounceRight(right: CGFloat) -> Bool {
+        let x = node.position.x
+        let limit = right - size
+        let beyond = x - limit
+        if beyond > 0 {
+            node.position.x = limit - beyond
+            return true
+        }
+        return false
+    }
+    
+    func maybeBounceX(right: CGFloat) {
+        if maybeBounceLeft() || maybeBounceRight(right: right) {
+            xSpeed = -xSpeed
+        }
+    }
+    
+    func maybeBounceBottom() -> Bool {
+        let y = node.position.y
+        let limit = CGFloat(0)
+        let beyond = limit - y
+        if beyond > 0 {
+            node.position.y = limit + beyond
+            return true
+        }
+        return false
+    }
+    
+    func maybeBounceTop(top: CGFloat) -> Bool {
+        let y = node.position.y
+        let limit = top - size
+        let beyond = y - limit
+        if beyond > 0 {
+            node.position.y = limit - beyond
+            return true
+        }
+        return false
+    }
+    
+    func maybeBounceY(top: CGFloat) {
+        if maybeBounceBottom() || maybeBounceTop(top: top) {
+            ySpeed = -ySpeed
+        }
     }
 
-    override func update(_ currentTime: TimeInterval) {
-        // calculate new position
-        var nextX = squareNode.position.x + xSpeed
-        var nextY = squareNode.position.y + ySpeed
-        var nextX2 = squareNode2.position.x + xSpeed2
-        var nextY2 = squareNode2.position.y + ySpeed2
-        
-        // handle collisions
-        let right = size.width - 50
-        let left = CGFloat(0)
-        let top = size.height - 50
-        let bottom = CGFloat(0)
-        
-        if nextX > right {
-            nextX = right
-            xSpeed = -xSpeed
-        }
-        if nextX < left {
-            nextX = left
-            xSpeed = -xSpeed
-        }
-        
-        if nextY > top {
-            nextY = top
-            ySpeed = -ySpeed
-        }
-        if nextY < bottom {
-            nextY = bottom
-            ySpeed = -ySpeed
-        }
-        
-        //
-        
-        if nextX2 > right {
-            nextX2 = right
-            xSpeed2 = -xSpeed2
-        }
-        if nextX2 < left {
-            nextX2 = left
-            xSpeed2 = -xSpeed2
-        }
-        
-        if nextY2 > top {
-            nextY2 = top
-            ySpeed2 = -ySpeed2
-        }
-        if nextY2 < bottom {
-            nextY2 = bottom
-            ySpeed2 = -ySpeed2
-        }
 
-        // update position
-        squareNode.position.x = nextX
-        squareNode.position.y = nextY
-        squareNode2.position.x = nextX2
-        squareNode2.position.y = nextY2
+    func maybeBounce(top: CGFloat, right: CGFloat) {
+        maybeBounceX(right: right)
+        maybeBounceY(top: top)
+    }
+}
+
+class GameScene: SKScene {
+    private var boxes : [Box] = []
+    
+    override func didMove(to view: SKView) {
+        var box = Box(
+            size: CGFloat(50),
+            xSpeed: CGFloat(3.0),
+            ySpeed: CGFloat(3.0),
+            position: CGPoint(x: 0, y: 0)
+        )
+        addChild(box.node)
+        boxes.append(box)
+        
+        box = Box(
+            size: CGFloat(50),
+            xSpeed: CGFloat(2.0),
+            ySpeed: CGFloat(1.3),
+            position: CGPoint(x: 80, y: 0)
+        )
+        addChild(box.node)
+        boxes.append(box)
+        
+        box = Box(
+            size: CGFloat(33),
+            xSpeed: CGFloat(3.0),
+            ySpeed: CGFloat(3.3),
+            position: CGPoint(x: 23, y: 90)
+        )
+        addChild(box.node)
+        boxes.append(box)
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        let top = size.height
+        let right = size.width
+
+        for box in boxes {
+            box.move()
+            box.maybeBounce(top: top, right: right)
+        }
     }
 }
